1b68d8b22366db052a07e6cfca4163056b1ea148
Niggo1299 <Niko.Klaesener@hsbi.de>
2026-05-26 21:01:01 +0200

    first commit

---
+++ main.m
@@ -0,0 +1,10 @@
+ks = knapsack(10,0,0);       
+items(5) = item;             
+
+items(1) = item("A",2,5,1);
+items(2) = item("B",3,7,1);
+items(3) = item("C",1,3,1);
+items(4) = item("D",4,9,1);
+items(5) = item("E",2,4,1);
+
+visualisation(ks, items)
